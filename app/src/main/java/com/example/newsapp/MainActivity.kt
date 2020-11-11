78ffd1c7fddcbc2cb99b76956d4025525c040c31
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-11-12 00:34:41 +0600

    Impelement demo

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        android.R.id
+        recycler_view.layoutManager = LinearLayoutManager(applicationContext)
+        val adapter: NewListAdapter = NewListAdapter(fetchDate())
+
+        recycler_view.adapter = adapter
+    }
+    private fun fetchDate(): ArrayList<String> {
+        val list = ArrayList<String>()
+        for(i in 0 until 100){
+            list.add("Item $i")
+        }
+        return list
     }
 }
